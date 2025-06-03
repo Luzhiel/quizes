b781746d69fbd4d54ee0915d50c239a47eee3a20
Luzhiel <ahsannurabdillah@gmail.com>
2025-06-03 10:08:14 +0700

    feat: menyambungkan database. soal dan jawaban sudah dinamis mengikuti database

--- src/components/QuizContent.vue
+++ src/components/QuizContent.vue
@@ -1,19 +1,25 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+// const { question } = defineProps<{
+//   question: {
+//     id: number;
+//     text: string;
+//     answers: string[];
+//   };
+// }>();
+
+const { question } = defineProps(['question'])
+</script>
+
 <template>
   <section class="mb-5">
-    <h2 class="text-3xl font-semibold">Siapa namanya?</h2>
+    <h2 class="text-3xl font-semibold">{{ question.text }}</h2>
   </section>
   <section>
-    <div class="flex mb-4 cursor-pointer"> <!-- option -->
-      <p class="py-2 px-3 align-center bg-sky-500 font-semibold">A.</p>
-      <div class="py-2 px-3 w-full bg-sky-200">Ahsan</div>
-    </div>
-    <div class="flex mb-4 cursor-pointer"> <!-- option -->
-      <p class="py-2 px-3 align-center bg-sky-500 font-semibold">B.</p>
-      <div class="py-2 px-3 w-full bg-sky-200">Zaidan</div>
-    </div>
-    <div class="flex mb-4 cursor-pointer"> <!-- option -->
-      <p class="py-2 px-3 align-center bg-sky-500 font-semibold">C.</p>
-      <div class="py-2 px-3 w-full bg-sky-200">Zulfikar</div>
+    <div v-for="option in question.answers" :key="option.id" class="flex mb-4 cursor-pointer"> <!-- option -->
+      <p class="py-2 px-3 align-center bg-sky-500 font-semibold">{{ option.label }}</p>
+      <div class="py-2 px-3 w-full bg-sky-200">{{ option.text }}</div>
     </div>
   </section>
 </template>
--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -2,10 +2,18 @@
 import { useRouter } from 'vue-router';
 import QuizHeader from '@/components/QuizHeader.vue';
 import QuizContent from '@/components/QuizContent.vue';
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+import quizes from '@/data/quizes.json';
 
+const route = useRoute();
+const quizId = parseInt(route.params.id as string, 10);
+const quiz = quizes.find((q) => q.id === quizId);
+
+const currentQuestionIndex = ref(0);
 </script>
 
 <template>
   <QuizHeader />
-  <QuizContent />
+  <QuizContent :question="quiz?.questions[currentQuestionIndex]" />
 </template>
